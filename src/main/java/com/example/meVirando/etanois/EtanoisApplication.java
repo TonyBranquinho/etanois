4ff90923d6e53106212e2e7a966d1cd40acad86c
Tony Branquinho <tony.branquinho@gmail.com>
2022-01-31 11:31:02 -0300

    comessando de novo

---
+++ src/main/java/com/example/meVirando/etanois/EtanoisApplication.java
@@ -0,0 +1,13 @@
+package com.example.meVirando.etanois;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EtanoisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EtanoisApplication.class, args);
+	}
+
+}
