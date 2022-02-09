4debe837eaae00ea5f4716567127ad52474eadad
Tony Branquinho <tony.branquinho@gmail.com>
2022-02-09 16:21:54 -0300

    Category

--- src/main/java/com/example/meVirando/etanois/config/TestConfig.java
+++ src/main/java/com/example/meVirando/etanois/config/TestConfig.java
@@ -8,9 +8,11 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
+import com.example.meVirando.etanois.entities.Category;
 import com.example.meVirando.etanois.entities.Order;
 import com.example.meVirando.etanois.entities.User;
 import com.example.meVirando.etanois.entities.enuns.OrderStatus;
+import com.example.meVirando.etanois.repositories.CategoryRepository;
 import com.example.meVirando.etanois.repositories.OrderRepository;
 import com.example.meVirando.etanois.repositories.UserRepository;
 
@@ -20,13 +22,22 @@ public class TestConfig implements CommandLineRunner {
 
 	@Autowired
 	private UserRepository userRepository;
-	
+
 	@Autowired
 	private OrderRepository orderRepository;
 
+	@Autowired
+	private CategoryRepository categoryRepository;
+
 	@Override
 	public void run(String... args) throws Exception {
-		// TODO Auto-generated method stub
+
+		Category cat1 = new Category(null, "Electronics");
+		Category cat2 = new Category(null, "Books");
+		Category cat3 = new Category(null, "Computers");
+
+		categoryRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
+		
 		User u1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456");
 		User u2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456");
 
@@ -35,6 +46,6 @@ public class TestConfig implements CommandLineRunner {
 		Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), OrderStatus.WAITING_PAYMENT, u1);
 		userRepository.saveAll(Arrays.asList(u1, u2));
 		orderRepository.saveAll(Arrays.asList(o1, o2, o3));
-		
+
 	}
 }
---
+++ src/main/java/com/example/meVirando/etanois/repositories/CategoryRepository.java
@@ -0,0 +1,9 @@
+package com.example.meVirando.etanois.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.meVirando.etanois.entities.Category;
+
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+
+}
